7c05a2104b19d94421df14736890468f18099d16
karimosman1210 <karimosman1210@gmail.com>
2018-04-14 09:40:28 +0200

    Prifile is Update

--- app/src/main/java/com/example/osman/grandresturant/UserProfile.java
+++ app/src/main/java/com/example/osman/grandresturant/UserProfile.java
@@ -1,24 +1,20 @@
 package com.example.osman.grandresturant;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Gallery;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,7 +22,6 @@ import android.widget.Toast;
 
 import com.example.osman.grandresturant.classes.Model_user;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +30,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
@@ -44,18 +40,23 @@ import java.io.InputStream;
 
 public class UserProfile extends AppCompatActivity {
     EditText phoneEditProfile, countryEditProfile, emailEditProfile, password_old, password_new, password_new2;
-    TextView tv_email, change_password;
-    ImageButton imageButton;
+    TextView tv_email, change_password, backTv;
+    ImageView imageButton;
     Button editBtnProfile;
     FirebaseAuth auth;
     DatabaseReference database;
-    ProgressBar myProgress;
-    LinearLayout linear_password;
-
+    ProgressBar progressall;
+    LinearLayout linear_password, allLiniar;
+    Uri imageUri;
     private static final int RUSLET_LOAD_IMAGE = 1;
-    static Uri image_item;
     private StorageReference mStorageReference;
+    boolean checkClick = false;
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,14 +69,17 @@ public class UserProfile extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance().getReference().child("Users");
         final String id_user = auth.getCurrentUser().getUid();
-        myProgress = (ProgressBar) findViewById(R.id.myProgress);
+        progressall = (ProgressBar) findViewById(R.id.progressall);
         tv_email = (TextView) findViewById(R.id.tv_email);
         password_old = (EditText) findViewById(R.id.password_old);
         password_new = (EditText) findViewById(R.id.password_new);
         password_new2 = (EditText) findViewById(R.id.password_new2);
-        imageButton = (ImageButton) findViewById(R.id.imagebutton_user);
+        imageButton = (ImageView) findViewById(R.id.imagebutton_user);
         linear_password = (LinearLayout) findViewById(R.id.Linear_password);
         change_password = (TextView) findViewById(R.id.change_password);
+        backTv = (TextView) findViewById(R.id.backTv);
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+        allLiniar = (LinearLayout) findViewById(R.id.allLiniar);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,7 +97,17 @@ public class UserProfile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 linear_password.setVisibility(View.VISIBLE);
+                change_password.setVisibility(View.INVISIBLE);
+                backTv.setVisibility(View.VISIBLE);
+                backTv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        linear_password.setVisibility(View.GONE);
+                        backTv.setVisibility(View.INVISIBLE);
+                        change_password.setVisibility(View.VISIBLE);
 
+                    }
+                });
             }
         });
 
@@ -107,6 +121,9 @@ public class UserProfile extends AppCompatActivity {
                 emailEditProfile.setText(model_user.getUsername());
                 countryEditProfile.setText(model_user.getCountry());
                 Picasso.with(UserProfile.this).load(model_user.getProfile_image()).into(imageButton);
+                progressall.setVisibility(View.INVISIBLE);
+                allLiniar.setVisibility(View.VISIBLE);
+
             }
 
             @Override
@@ -119,56 +136,84 @@ public class UserProfile extends AppCompatActivity {
         editBtnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (linear_password.getVisibility() == View.VISIBLE) {
-                    change();
-                } else if (!password_new2.getText().toString().isEmpty()) {
-
-                } else {
 
 
-                    final AlertDialog.Builder builder = new AlertDialog.Builder(UserProfile.this);
-                    builder.setMessage("هل انت متاكد من تعديل معلوماتك الشخصيه");
-
-                    builder.setPositiveButton("نعم", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
+                try {
 
 
-                            myProgress.setVisibility(View.VISIBLE);
-                            String phone = phoneEditProfile.getText().toString().trim();
-                            String country = countryEditProfile.getText().toString().trim();
-                            String email = emailEditProfile.getText().toString().trim();
-                        // database.child(id_user).child("profile_image").setValue(imageUri.toString());
-                            database.child(id_user).child("country").setValue(country);
-                            database.child(id_user).child("username").setValue(email);
-                            database.child(id_user).child("mobile").setValue(phone);
-                            myProgress.setVisibility(View.INVISIBLE);
+                    if (linear_password.getVisibility() == View.VISIBLE) {
+                        change();
+                    } else if (!password_new2.getText().toString().isEmpty()) {
+
+                    } else {
 
 
-                            finish();
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(UserProfile.this);
+                        builder.setMessage("هل انت متاكد من تعديل معلوماتك الشخصيه");
 
-                        }
-                    });
+                        builder.setPositiveButton("نعم", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
 
-                    builder.setNegativeButton("لا", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //perform any action
-                        }
-                    });
 
-                    //creating alert dialog
-                    AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
+                                String phone = phoneEditProfile.getText().toString().trim();
+                                String country = countryEditProfile.getText().toString().trim();
+                                String email = emailEditProfile.getText().toString().trim();
+                                // database.child(id_user).child("profile_image").setValue(imageUri.toString());
+
+                                database.child(id_user).child("country").setValue(country);
+                                database.child(id_user).child("username").setValue(email);
+                                database.child(id_user).child("mobile").setValue(phone);
+                                try {
+                                    if (checkClick == true) {
+                                        progressall.setVisibility(View.VISIBLE);
+                                        mStorageReference.child(id_user).child("profile.jpg").putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                                                if (task.isSuccessful()) {
+
+                                                    database.child(id_user).child("profile_image").setValue(task.getResult().getDownloadUrl().toString());
+                                                    progressall.setVisibility(View.INVISIBLE);
+                                                    Toast.makeText(UserProfile.this, "تم التعديل", Toast.LENGTH_SHORT).show();
+                                                    progressall.setVisibility(View.INVISIBLE);
+                                                    finish();
+
+                                                }
+                                            }
+                                        });
+                                    }
+
+
+                                } catch (Exception e) {
+
+                                }
+
+                            }
+
+
+                        });
+
+
+                        builder.setNegativeButton("لا", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        //creating alert dialog
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+
+                    }
+
+                } catch (Exception e) {
 
                 }
 
             }
-
         });
-
-
-
 
 
     }
@@ -200,6 +245,7 @@ public class UserProfile extends AppCompatActivity {
             Toast.makeText(this, "اعد كتابه الباسورد صحيح", Toast.LENGTH_SHORT).show();
         }
     }
+
     @Override
     protected void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
@@ -207,19 +253,22 @@ public class UserProfile extends AppCompatActivity {
 
         if (resultCode == RESULT_OK) {
             try {
-                final Uri imageUri = data.getData();
+                imageUri = data.getData();
+                checkClick = true;
                 final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+
                 final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                 imageButton.setImageBitmap(selectedImage);
-                database.child(auth.getCurrentUser().getUid()).child("profile_image").setValue(imageUri.toString());
+
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
                 Toast.makeText(UserProfile.this, "Something went wrong", Toast.LENGTH_LONG).show();
             }
 
-        }else {
-            Toast.makeText(UserProfile.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(UserProfile.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
         }
     }
 }
+
