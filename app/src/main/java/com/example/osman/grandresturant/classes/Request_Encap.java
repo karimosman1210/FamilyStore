625f894ba53c1657368b5846c5ddf0c10fa68895
karimosman1210 <karimosman1210@gmail.com>
2018-05-01 02:54:41 +0200

    request#1

---
+++ app/src/main/java/com/example/osman/grandresturant/classes/Request_Encap.java
@@ -0,0 +1,112 @@
+package com.example.osman.grandresturant.classes;
+
+/**
+ * Created by osman on 5/1/2018.
+ */
+
+public class Request_Encap {
+    String requestItemID;
+    String requestItemImage;
+    String requestItemName;
+    String requestItemPrice;
+    String requestSallerID;
+
+    public Request_Encap() {
+    }
+
+    public String getRequestItemID() {
+        return requestItemID;
+    }
+
+    public void setRequestItemID(String requestItemID) {
+        this.requestItemID = requestItemID;
+    }
+
+    public String getRequestItemImage() {
+        return requestItemImage;
+    }
+
+    public void setRequestItemImage(String requestItemImage) {
+        this.requestItemImage = requestItemImage;
+    }
+
+    public String getRequestItemName() {
+        return requestItemName;
+    }
+
+    public void setRequestItemName(String requestItemName) {
+        this.requestItemName = requestItemName;
+    }
+
+    public String getRequestItemPrice() {
+        return requestItemPrice;
+    }
+
+    public void setRequestItemPrice(String requestItemPrice) {
+        this.requestItemPrice = requestItemPrice;
+    }
+
+    public String getRequestSallerID() {
+        return requestSallerID;
+    }
+
+    public void setRequestSallerID(String requestSallerID) {
+        this.requestSallerID = requestSallerID;
+    }
+
+    public String getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(String requestTime) {
+        this.requestTime = requestTime;
+    }
+
+    public String getRequestUserEmail() {
+        return requestUserEmail;
+    }
+
+    public void setRequestUserEmail(String requestUserEmail) {
+        this.requestUserEmail = requestUserEmail;
+    }
+
+    public String getRequestUserID() {
+        return requestUserID;
+    }
+
+    public void setRequestUserID(String requestUserID) {
+        this.requestUserID = requestUserID;
+    }
+
+    public String getRequestUserName() {
+        return requestUserName;
+    }
+
+    public void setRequestUserName(String requestUserName) {
+        this.requestUserName = requestUserName;
+    }
+
+    public String getRequestUserNumber() {
+        return requestUserNumber;
+    }
+
+    public void setRequestUserNumber(String requestUserNumber) {
+        this.requestUserNumber = requestUserNumber;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    String requestTime;
+    String requestUserEmail;
+    String requestUserID;
+    String requestUserName;
+    String requestUserNumber;
+    String quantity;
+
+}
